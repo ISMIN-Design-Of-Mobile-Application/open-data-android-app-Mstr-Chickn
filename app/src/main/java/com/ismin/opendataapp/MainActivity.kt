8a59589c8e2d9f2a6b7419646b410a900af647e9
AnthouardB <benjamin.anthouard@orange.fr>
2019-12-01 18:57:51 +0100

    Fragments
    Ajout : Le fragment Info fonctionne
    Ajout : Le fragment Carte plus compliqué

--- app/src/main/java/com/ismin/opendataapp/MainActivity.kt
+++ app/src/main/java/com/ismin/opendataapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.ismin.opendataapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttInfo = findViewById<Button>(R.id.info_button)
+        buttInfo.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            val fragment = InfoFragment()
+            fragmentTransaction.replace(R.id.main_fragments_view, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
